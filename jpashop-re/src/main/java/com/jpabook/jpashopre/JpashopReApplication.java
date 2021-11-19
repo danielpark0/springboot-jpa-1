2b884b2deea9378ead4e833fb5f0e0bb5a97878f
danielpark0 <37523983+danielpark0@users.noreply.github.com>
2021-11-19 23:11:42 +0900

    init

---
+++ jpashop-re/src/main/java/com/jpabook/jpashopre/JpashopReApplication.java
@@ -0,0 +1,13 @@
+package com.jpabook.jpashopre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopReApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpashopReApplication.class, args);
+	}
+
+}
---
+++ jpashop-re/src/test/java/com/jpabook/jpashopre/JpashopReApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpabook.jpashopre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopReApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
