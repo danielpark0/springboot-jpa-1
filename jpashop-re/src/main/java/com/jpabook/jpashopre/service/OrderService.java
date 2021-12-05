aa4a97c4f8ac5be3adbe1fecbefcdcd0be1d9fa7
danielpark0 <37523983+danielpark0@users.noreply.github.com>
2021-12-05 12:10:03 +0900

    order Service

---
+++ jpashop-re/src/main/java/com/jpabook/jpashopre/service/OrderService.java
@@ -0,0 +1,65 @@
+package com.jpabook.jpashopre.service;
+
+import com.jpabook.jpashopre.domain.Delivery;
+import com.jpabook.jpashopre.domain.Member;
+import com.jpabook.jpashopre.domain.Order;
+import com.jpabook.jpashopre.domain.OrderItem;
+import com.jpabook.jpashopre.domain.item.Item;
+import com.jpabook.jpashopre.repository.ItemRepository;
+import com.jpabook.jpashopre.repository.MemberRepository;
+import com.jpabook.jpashopre.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+    /**
+     * 주문
+     */
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+
+        //엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        //배송정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        //주문상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        //주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        //주문 저장
+        orderRepository.save(order);
+
+        return order.getId();
+    }
+
+    /**
+     * 취소
+     */
+    @Transactional
+    public void cancelOrder(Long orderId) {
+        Order order = orderRepository.findOne(orderId);
+        order.cancel();
+    }
+
+    //검색
+//    public List<OrderItem> findOrders(OrderSearch orderSearch) {
+//        return orderRepository.findAll(orderSearch);
+//    }
+}
