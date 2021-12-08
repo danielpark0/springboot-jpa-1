1b7b64341cfd7e11cfad7b1d2e6b884cb46d3e58
danielpark0 <37523983+danielpark0@users.noreply.github.com>
2021-12-08 21:42:03 +0900

    home, layout

--- jpashop-re/src/main/java/com/jpabook/jpashopre/repository/OrderRepository.java
+++ jpashop-re/src/main/java/com/jpabook/jpashopre/repository/OrderRepository.java
@@ -25,7 +25,7 @@ public class OrderRepository {
 
     public List<Order> findAllbyString(OrderSearch orderSearch) {
         String jpql = "select o From Order o join o.member m";
-        
+
         boolean isFirstCondition = true;
 
         //주문 상태 검색
