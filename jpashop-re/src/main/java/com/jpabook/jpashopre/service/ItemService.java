04f9dcc7665826665efce20953e30ee682c6ff10
danielpark0 <37523983+danielpark0@users.noreply.github.com>
2022-02-15 21:18:37 +0900

    변경감지 병합

--- jpashop-re/src/main/java/com/jpabook/jpashopre/service/ItemService.java
+++ jpashop-re/src/main/java/com/jpabook/jpashopre/service/ItemService.java
@@ -1,5 +1,6 @@
 package com.jpabook.jpashopre.service;
 
+import com.jpabook.jpashopre.domain.item.Book;
 import com.jpabook.jpashopre.domain.item.Item;
 import com.jpabook.jpashopre.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,14 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
     }
 
     public List<Item> findItems() {
